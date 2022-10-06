70a185d71f89e279a32fd8017adb557801734775
Budaházi Bence <budahazi.bence@petrik.hu>
2022-10-06 08:38:56 +0200

    Véletlen osztály - statikus viselkedés:   - az osztály final   - privát konstruktor - Random adattag - Listák nevekkel - véletlen egész
    Main
    - véletlen egész tesztje

--- src/hu/petrik/statikusosztalyok/Main.java
+++ src/hu/petrik/statikusosztalyok/Main.java
@@ -1,7 +1,21 @@
 package hu.petrik.statikusosztalyok;
 
 public class Main {
-    public static void main(String[] args) {
+    /*
+    int nemStatikusAdattag;
+    static int statikusTag;
+    */
 
+    public static void main(String[] args) {
+        /*
+        Main objektum = new Main();
+        System.out.println(objektum.nemStatikusAdattag);
+        System.out.println(Main.statikusTag);
+        Math math = new Math();
+        */
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Veletlen.velEgesz(5, 10));
+        }
     }
 }
